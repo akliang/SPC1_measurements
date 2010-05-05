77539a99f5f1683a7da8e72bd8b648f3670a1743
koniczek <koniczek@92890ac3-fb6f-4dbc-9a08-888296f68159>
2010-05-05 21:51:29 +0000

    still work in progress on timing estimation, this is a buggy/non-functional version
    git-svn-id: svn://svn.imager.umro/masdax/trunk/scripts/scriptmeas@65 92890ac3-fb6f-4dbc-9a08-888296f68159

--- tool_notification.m
+++ tool_notification.m
@@ -1,16 +1,23 @@
-function [error] = tool_notification(on,meas,finished,long)
+function [error] = tool_notification(on,env,meas,multi,finished,long)
 % XMPP network notification tool to help monitor script progress
 % "on" is a flag to send a message
 %    - set to flag.first_run to notify only once a start of run
 %    - set to 1 to notify at every step of the experiment
-% "user" is the username of the jabber acct (note: pw is hardcoded)
+% "env" is the env.struct
 % "meas" is the meas.struct
 % "multi" is the multi.struct
+% "finished" is flag.finished for msg to say "started" or "finished"
 % "long" is a binary-sequence (converted to base10) to control output
 %  000 = ['R variables' 'voltage variables']
 %  ex. long = 2 = '010' = display R variables
 %  ex. long = 5 = '101' = display estimated time and voltage variables
 
+% time_step for time estimation
+if (env.G3ExtClock==1000000) % no external clock
+    time_step = 0.00062;
+else                         % assuming 100khz clk
+    time_step = 0.0062;
+end    
 
 if (on == 1)
     % decode the "long" string to determine what user wants
@@ -45,12 +52,19 @@ if (on == 1)
         msg = [msg ' \n ' msg_temp];
     end
 
-
     % determine if it is "start" or "finish"
     if (finished)
         msg = [msg ' finished'];
     else
         msg = [msg ' started'];
+        % calculate the estimated time to completion
+        [rows cols] = size(multi.RMATRIX);
+        time_sum = 0;
+        for i=1:rows
+            time_sum = time_sum + multi.RMATRIX(i,1)*(multi.RMATRIX(i,2)+multi.RMATRIX(i,3));
+        end
+        time_sum = num2str(round(time_step * time_sum / 60));
+        msg = [msg ' (' time_sum ' mins)']
     end
     
     
@@ -60,5 +74,5 @@ if (on == 1)
     username = sprintf('%s.ubuntu',username);
     msg = [username '/' msg];
     
-    system(['echo -e "' msg '" | sendxmpp -u ' username ' -p masda -j jabber.imager.umro --chatroom argus@conference.jabber.imager.umro']);
+    %system(['echo -e "' msg '" | sendxmpp -u ' username ' -p masda -j jabber.imager.umro --chatroom argus@conference.jabber.imager.umro']);
 end
