b3dd9b7c9d6344bdf19442d4bd04525aca0af582
koniczek <koniczek@92890ac3-fb6f-4dbc-9a08-888296f68159>
2011-04-19 19:25:29 +0000

    multim_2636A_sweepctrl.sh - more flexible RST off conditions
    git-svn-id: svn://svn.imager.umro/masdax/trunk/scripts/scriptmeas@304 92890ac3-fb6f-4dbc-9a08-888296f68159

--- multim_2636A_sweepctrl.sh
+++ multim_2636A_sweepctrl.sh
@@ -579,17 +579,19 @@ function sfchar_triple() { # Full SF characterization for 29B-1 TAA
   if true; then
   # Load a voltage onto Cpix and perform multiple different sequences
   VRST=6
+  RSTLO=3
+  RSTHI=15
   for TYPE in 'PreSwitch' 'None' 'CancelInj' 'Vbias' 'ResetP' 'ResetN' 'VresetP' 'VresetN' ; do
   TL=20
   send_cmd "v3($VRST)"
-  send_cmd "v1(15)"
+  send_cmd "v1($RSTHI)"
   send_cmd "v5(1)"
   echo "Vreset$VRST"_"pulsing$TYPE"_"ch2src=$SOURCEMODE" >"$DATACTRLFILE"
   read -t $TB N 
   if [ "$TYPE" == "PreSwitch" ]; then
     for SW in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
-      send_cmd "v1(0)"
-      send_cmd "v1(15)"
+      send_cmd "v1($RSTLO)"
+      send_cmd "v1($RSTLHI)"
     done
   else
   # show that reading is not influenced by some switching
@@ -601,9 +603,9 @@ function sfchar_triple() { # Full SF characterization for 29B-1 TAA
 
   # turn TFTreset OFF 
   if [ "$TYPE" == "CancelInj" ]; then
-  send_cmd "v1(0) v5(1.2)"
+  send_cmd "v1($RSTLO) v5(1.2)"
   else
-  send_cmd "v1(0)"
+  send_cmd "v1($RSTLO)"
   fi
   read -t $TL N 
 
@@ -611,8 +613,8 @@ function sfchar_triple() { # Full SF characterization for 29B-1 TAA
   for PULSES in 1 2 3 4 5 6; do
   if [ "$TYPE" == 'None'    ]; then send_cmd "";            fi
   if [ "$TYPE" == 'Vbias'   ]; then send_cmd "v5(0)";       fi
-  if [ "$TYPE" == 'ResetN'  ]; then send_cmd "v1(-1)";      fi
-  if [ "$TYPE" == 'ResetP'  ]; then send_cmd "v1(1)";       fi
+  if [ "$TYPE" == 'ResetN'  ]; then send_cmd "v1($RSTLO-1)";      fi
+  if [ "$TYPE" == 'ResetP'  ]; then send_cmd "v1($RSTLO+1)";       fi
   if [ "$TYPE" == 'VresetN' ]; then send_cmd "v3($VRST-1)"; fi
   if [ "$TYPE" == 'VresetP' ]; then send_cmd "v3($VRST+1)"; fi
   #send_cmd "v2(1) v4(1) v6(1)"
@@ -620,8 +622,8 @@ function sfchar_triple() { # Full SF characterization for 29B-1 TAA
   read -t $TL N 
   if [ "$TYPE" == 'None'    ]; then send_cmd "";            fi
   if [ "$TYPE" == 'Vbias'   ]; then send_cmd "v5(1)";       fi
-  if [ "$TYPE" == 'ResetN'  ]; then send_cmd "v1(0)";       fi
-  if [ "$TYPE" == 'ResetP'  ]; then send_cmd "v1(0)";       fi
+  if [ "$TYPE" == 'ResetN'  ]; then send_cmd "v1($RSTLO)";       fi
+  if [ "$TYPE" == 'ResetP'  ]; then send_cmd "v1($RSTLO)";       fi
   if [ "$TYPE" == 'VresetN' ]; then send_cmd "v3($VRST)";   fi
   if [ "$TYPE" == 'VresetP' ]; then send_cmd "v3($VRST)";   fi
   read -t $TL N 
