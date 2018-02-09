5f1258e0e60ad3339ec4a37631261bfb48e8882d
User <user@measdelli.(none)>
2018-02-09 18:40:31 -0500

    akl: improved generate_colormap to more closely mimic the PMB paper colormaps

--- SPC/generate_colormap.m
+++ SPC/generate_colormap.m
@@ -1,27 +1,44 @@
 
 function generate_colormap(csvfile)
 
+  [filepath,filename,fileext]=fileparts(csvfile);
   q=load(csvfile);
 
-  m2b=sort(unique(q(:,1)));
-  m3b=sort(unique(q(:,2)));
-  m4b=sort(unique(q(:,3)));
+  s.measid=1;
+  s.m2br=2;
+  s.m3br=3;
+  s.m4br=4;
+  s.vcc=5;
+  s.gnd=6;
+  s.m2b=7;
+  s.m3b=8;
+  s.m4b=9;
+  s.math1min=10;
+  s.math1max=11;
+  s.math2min=12;
+  s.math2max=13;
 
-  gain=(q(:,12)-q(:,11)) ./ (q(:,10)-q(:,9));
+  m2b=sort(unique(q(:,s.m2br)));
+  m3b=sort(unique(q(:,s.m3br)));
+  m4b=sort(unique(q(:,s.m4br)));
+
+  gain=(q(:,s.math2max)-q(:,s.math2min)) ./ (q(:,s.math1max)-q(:,s.math1min));
   gain=reshape(gain,[numel(m4b) numel(m2b)]);
+  gain=gain';  % transpose to match the orientation of the simulations
 
-  imagesc(m2b,m4b,gain); colorbar
+  fh=figure();
+  imagesc(m4b,m2b,gain); colorbar
   set(gca,'YDir','normal');
-  title('Amp gain in response to sine wave')
-  xlabel('m2b (V)')
-  ylabel('m4b (V)')
+  xlabel('m4b (V)')
+  ylabel('m2b (V)')
 
-  sprintf('max gain: %0.2f',max(max(gain)))
+  % find the max gain and bias settings
+  maxgain=max(max(gain));
+  [r c]=find(gain==maxgain);
 
-
-
-
-
+  % save the figure to a PNG
+  title(sprintf('Amp gain in response to sine wave (maxgain=%0.2f at m2b=%0.2f and m4b=%0.2f)',maxgain,m2b(r),m4b(c)))
+  print(fh,sprintf('%s/colormap_gain.png',filepath));
 
 end % end-function
 
