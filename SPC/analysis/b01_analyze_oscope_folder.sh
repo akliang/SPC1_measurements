f39cc113a6fa69e8b0b35bab37761ff94a327823
none <none@none.com>
2018-09-19 17:45:15 -0400

    updated both b0x scripts to be compatible with c01 analysis method

--- SPC/analysis/b01_analyze_oscope_folder.sh
+++ SPC/analysis/b01_analyze_oscope_folder.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-INDIR="$1"
-if [ "$INDIR" == "" ]; then
-  echo "Needs an input folder"
-fi
+INDIR=$1
 
-
-for F in $( ls -1d $INDIR/meas* ); do 
-  MEASID=$( echo "$F" | sed -e "s/.*meas//" -e "s/\([0-9]*\).*/\1/" )
-  SMUDAT=$( head -n 1 $F/environment.txt | gawk '{ printf "%0.2f\t%0.2f\t%0.2f", $8,$10,$12 }' )
-  #AMPDAT=$( echo "b02_analyze_oscope_amp('$F')" | octave -qH )
-  AMPDAT=$( echo "b02_analyze_oscope_amp('$F')" | matlab )
-  echo -e "$MEASID\t$SMUDAT\t$AMPDAT"
+# clean the data (needed for matlab scripts)
+for F in $( ls -1d $INDIR/*.csv ); do
+  ./helper_clean_csv.sh $F $F.clean
 done
 
+# grab voltage values from environment script
+MEASID=$( echo "$INDIR" | sed -e "s/.*meas//" -e "s/\([0-9]*\).*/\1/" )
+SMUDAT=$( head -n 1 $INDIR/environment.txt | awk '{ printf "%0.2f\t%0.2f\t%0.2f", $8,$10,$12 }' )
 
+# this is disabled because c01 calls it explicitly
+#AMPDAT=$( echo "b02_analyze_oscope_amp('$F')" | octave -qH )
+#AMPDAT=$( echo "b02_analyze_oscope_amp('$F')" | matlab )
+
+# echo the values for c01 to grab
+#echo -e "$MEASID\t$SMUDAT\t$AMPDAT"
+echo -e "$MEASID\t$SMUDAT"
 
--- SPC/analysis/b02_analyze_oscope_amp.m
+++ SPC/analysis/b02_analyze_oscope_amp.m
@@ -3,7 +3,7 @@ function [amplifier_input,amplifier_output,settling_time]=b02_analyze_oscope_amp
   %measdir='/Volumes/ArrayData/MasdaX/2018-01/measurements/20180306T185749/meas001/';
   %measnum=regexprep(measdir,'.*meas','');
   %measnum=regexprep(measnum,'/$','');
-  
+
   % assuming input and output csv files
   incsv=[measdir '/math1.csv.clean'];
   if(~exist(incsv))
@@ -17,11 +17,28 @@ function [amplifier_input,amplifier_output,settling_time]=b02_analyze_oscope_amp
   invals=in(:,2);
   outtime=out(:,1);
   outvals=out(:,2);
+
+  % examine only the falling-edge part of the data
+  % find the idx of the falling edge
+  invals_mean=mean(invals);
+  invals_binary=(invals>invals_mean);
+  invals_diff=diff(invals_binary);
+  % find the falling and rising edges
+  in_falling_idx = find(invals_diff == -1);
+  if (numel(in_falling_idx) > 1); warning('More than 1 falling edge found, taking the first one'); in_falling_idx=in_falling_idx(1); end
+  in_rising_idx = find(invals_diff == 1);
+  if (numel(in_rising_idx) > 1); warning('More than 1 rising edge found, taking the first one'); in_rising_idx=in_rising_idx(1); end
+  % truncate the signal
+  idx_range = in_falling_idx-10 : in_rising_idx-10;
+  invals=invals(idx_range);
+  outtime=outtime(idx_range);
+  outvals=outvals(idx_range);
   
+
   % find the index where invals is largest
   [tmp idx]=max(abs(diff(invals)));
   idx=idx-1;  % try to shift back a little bit to find the true start
-  
+
   % figure out the input size
   % note: this is assuming the input pulse steps downwards
   amplifier_input=in(idx,2)-min(in(:,2));
@@ -46,23 +63,29 @@ function [amplifier_input,amplifier_output,settling_time]=b02_analyze_oscope_amp
   if (numel(crossBelow) == 0); crossBelow=1; end
   % take the right-most value of the two
   if (crossAbove(end) > crossBelow(end))
-      settling_idx = crossAbove(end);
+    settling_idx = crossAbove(end);
   else
-      settling_idx = crossBelow(end);
+    settling_idx = crossBelow(end);
   end
   settling_time = outtime(settling_idx) - outtime(idx);
-  
-  plot(outtime,outvals)
-  hold on
-  plot(outtime,outvals_smooth,'r')
-  plot([outtime(1) outtime(end)],[baseV+settling_time_threshold baseV+settling_time_threshold],'b')
-  plot([outtime(1) outtime(end)],[baseV-settling_time_threshold baseV-settling_time_threshold],'g')
-  if (numel(crossAbove)>0); plot(outtime(crossAbove) , outvals_smooth(crossAbove) , 'bo'); end
-  if (numel(crossBelow)>0); plot(outtime(crossBelow) , outvals_smooth(crossBelow) , 'gx'); end
-  hold off
-  
-  
-  % output results
-  fprintf(1,'amp_input: %f\tamp_output: %f\tsettling_time: %e\tcount_rate: %g\n',amplifier_input,amplifier_output,settling_time,1/settling_time);
+
+  % plot stuff
+  if false
+    plot(outtime,outvals)
+    hold on
+    plot(outtime,invals,'y')
+    plot(outtime,outvals_smooth,'r')
+    plot([outtime(1) outtime(end)],[baseV+settling_time_threshold baseV+settling_time_threshold],'b')
+    plot([outtime(1) outtime(end)],[baseV-settling_time_threshold baseV-settling_time_threshold],'g')
+    if (numel(crossAbove)>0); plot(outtime(crossAbove) , outvals_smooth(crossAbove) , 'bo'); end
+    if (numel(crossBelow)>0); plot(outtime(crossBelow) , outvals_smooth(crossBelow) , 'gx'); end
+    plot(outtime(idx),outvals_smooth(idx),'ks')
+    hold off
+    legend('outvals','invals','outvals smooth','threshold lower','threshold higher','values above','values below','idx point')
+    title(sprintf('inval at idx: %f ; inval min: %f ; outval baseV: %f ; outval maxV: %f',in(idx,2),min(in(:,2)),mean(out(1:idx,2)),max(out(:,2))));
+  end
+
+  % output results to terminal
+  %fprintf(1,'amp_input: %f\tamp_output: %f\tsettling_time: %e\tcount_rate: %g\n',amplifier_input,amplifier_output,settling_time,1/settling_time);
   
 end % end-function
