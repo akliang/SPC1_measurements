64823478bdbb81dc34c499b38185319f877e20d0
none <none@none.com>
2020-01-30 10:12:14 -0500

    added mi.close command just in case it conflicts with oscope mi handle, added notes for how to implement query-then-execute in the future

--- SPC/helpers/siggen_functions.py
+++ SPC/helpers/siggen_functions.py
@@ -2,7 +2,7 @@
 import visa
 
 
-def run(query):
+def send_query(query):
     # send commands to the siggen
     # query syntax:
     # query = [
@@ -13,6 +13,7 @@ def run(query):
     # *RCL 3 (restore instrument settings from mem location 3)
     # *SAV 3 (save setting to mem location 3)
     # *RST (set siggen to Default)
+    # OUTPUT[12]:STATE ON
     # SOURCE[12]:FUNCTION:SHAPE [sinusoid, square, pulse, ramp, user1]
     # SOURCE[12]:VOLTAGE:LEVEL:IMMEDIATE:HIGH 1V
     # SOURCE[12]:VOLTAGE:LEVEL:IMMEDIATE:LOW  1V
@@ -27,11 +28,33 @@ def run(query):
     mi = rm.open_resource("TCPIP::" + siggenip + "::INSTR")
 
     # send command(s)
+    # TODO: query first then execute
+    # query = [
+    #     [0, "OUTPUT1:STATE OFF"],
+    #     [0, "OUTPUT2:STATE OFF"],
+    #     [1, "SOURCE1:VOLTAGE:UNIT VPP"],
+    #     [1, "SOURCE1:FUNCTION:SHAPE RAMP"],
+    #     [1, "SOURCE1:VOLTAGE:LEVEL:IMMEDIATE:HIGH %fV" % vhi],
+    #     [1, "SOURCE1:VOLTAGE:LEVEL:IMMEDIATE:LOW  %fV" % vlo],
+    #     [0, "OUTPUT1:STATE ON"],
+    # ]
+    #
+    # for q in query:
+    #     if q[0] == 0:
+    #         print("immediately executing: %s" % q[1])
+    #     elif q[0] == 1:
+    #         t = q[1].split()
+    #         t = "%s?" % t[0]
+    #         print("querying first with: %s" % t)
     for q in query:
         if q[0].endswith("?"):
             print(mi.query(q[0]))
+            print(q[0])
         else:
             mi.write(q[0])
+            print(q[0])
+
+    mi.close()
 
 
 
