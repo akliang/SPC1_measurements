9be192f2bbe7ec9fd6243f2cae48986f69ed5faf
none <none@none.com>
2020-03-10 13:38:48 -0400

    massive analysis script improvements to generate AAPM abstract figures

--- SPC/analysis/analyze_amplifier_meas.m
+++ SPC/analysis/analyze_amplifier_meas.m
@@ -5,6 +5,7 @@
 % 3. (DONE) delete the duplicate process_oscope_data_helper.sh script (there is one in the helper_fxns directory too)
 % 4. (DONE) vbiases.txt file has been updated to incorporate ALL voltages (so need to update the columns used below accordingly)
 % 5. Remove gainfac
+% 6. Which settling time percentage to use?  5% or 1%?
 
 % analysis folder
 global gainfac;  % temporary patch for mis-atten data, delete after 20191119T101410 is no longer needed
@@ -19,14 +20,19 @@ global gainfac;  % temporary patch for mis-atten data, delete after 20191119T101
 
 % new data taken with improved acq script and on wafer5
 %ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200212T172727_29D1-5_WP5_1-1-1_amp3st1bw/'; gainfac=1;
-ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200214T174201_29D1-5_WP5_1-1-1_amp3st1bw/'; gainfac=1;
+%ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200214T174201_29D1-5_WP5_1-1-1_amp3st1bw/'; gainfac=1;
+ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200217T170121_29D1-5_WP5_1-1-1_amp3st1bw/'; gainfac=1;
+ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200219T104707_29D1-5_WP5_1-1-1_amp3st1bw/'; gainfac=1;
 
 addpath('./helper_functions');
 % clean the oscope data to make it matlab-friendly
 clean_oscope_data(ana_folder);
 vb_dat = load([ana_folder '/vbiases.txt']);
 
+%%{
 % walk through every meas and analyze
+global waveform_save;
+waveform_save={};
 ana_res_all = zeros([size(vb_dat,1) 3]);
 for fidx=1:size(vb_dat,1)
     if (mod(fidx,10)==0)
@@ -42,11 +48,17 @@ end
 alldat = [vb_dat ana_res_all];
 resfile = [ana_folder '/results.txt' ];
 csvwrite(resfile,alldat);
+%}
 
 
-% generate the visual colormap of the overall results
+% find optimal point and generate the visual colormaps
 [m2b, m4b, gain, outV]=generate_colormap(ana_folder);
 
+% debug line
+%[amplifier_input,amplifier_output,settling_time]=analyze_oscope_amp([ana_folder '/meas0345']);
+
+%meas_to_rc_translator(ana_folder,[-.7, .75])
+%plot_specific_meas(ana_folder,meas_to_rc_translator(ana_folder,[-.7, .75]));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %    analysis functions    %
@@ -54,6 +66,12 @@ csvwrite(resfile,alldat);
 function [amplifier_input,amplifier_output,settling_time]=analyze_oscope_amp(measdir)
 
   debug=false;
+
+  global waveform_save
+  global wss
+
+  %settling_time_threshold = 0.05;
+  settling_time_percent = 0.01;
   
   % assuming input and output csv files
   incsv=[measdir '/math1.csv.clean'];
@@ -61,16 +79,21 @@ function [amplifier_input,amplifier_output,settling_time]=analyze_oscope_amp(mea
     incsv=[measdir '/ch1.csv.clean'];
   end
   outcsv=[measdir '/math2.csv.clean'];
+  trigcsv=[measdir '/ch4.csv.clean'];
   % load the files
   in=load(incsv);
   out=load(outcsv);
+  trig=load(trigcsv);
   % define signal variables
   invals=in(:,2);
   outtime=out(:,1);
   outvals=out(:,2);
+  trigger=trig(:,2);
 
   % method: examine only the falling-edge part of the data
   % find the idx of the falling edge
+  % note: this code was retired on 2020-02-24 in favor of using siggen trigger on ch4
+  %{
   invals_mean=mean(invals);
   invals_binary=(invals>invals_mean);
   invals_diff=diff(invals_binary);
@@ -83,12 +106,18 @@ function [amplifier_input,amplifier_output,settling_time]=analyze_oscope_amp(mea
   if (numel(in_falling_idx) < 3)
       error('Less than 3 falling edges found, cannot run analysis');
   end
+  %}
+  trig_mean = mean(trigger);
+  % find the places where the signal crosses
+  trig_cross = diff(trigger>trig_mean);
+  trig_rising_idx = find(trig_cross == 1);
   
   % truncate the signal to a single waveform
-  idx_range = in_falling_idx(2)-30 : in_falling_idx(3)-10;
+  idx_range = trig_rising_idx(2)-30 : trig_rising_idx(3)-10;
   invals_single=invals(idx_range);
   outtime_single=outtime(idx_range);
   outvals_single=outvals(idx_range);
+  trigger_single=trigger(idx_range);
  
   % find the index where invals is largest
   [~, idx]=max(abs(diff(invals_single)));
@@ -100,65 +129,75 @@ function [amplifier_input,amplifier_output,settling_time]=analyze_oscope_amp(mea
   baseV = mean(outvals_single(1:idx));
   % figure out the output size (normalized to baseV)
   amplifier_output = max(outvals_single) - baseV;
+  amplifier_underswing = abs(min(outvals_single) - baseV);
   
-  
-  % calculate settling time
-  % first attempt... smooth with 1/20th width sliding window
-  % note: this makes amp-max very inaccurate.
-  %       do not use outvals_smooth to derive amp-max!!
-  smooth_span = round(numel(outvals_single)/20);
-  outvals_smooth = movmean(outvals_single,smooth_span);
-  % define the threshold (absolute value)
-  settling_time_threshold = abs(amplifier_output*0.05);
-  % the find point where it crosses above/below the threshold
-  crossAbove = find(outvals_smooth > (baseV+settling_time_threshold) );
-  crossBelow = find(outvals_smooth < (baseV-settling_time_threshold) );
-  % note: both variables SHOULD contain an array of index values, but if
-  % could also return empty, so ensure they always have a value
-  % perhaps this should throw an error instead?
-  if (numel(crossAbove) == 0); crossAbove=1; end
-  if (numel(crossBelow) == 0); crossBelow=1; end
-  % take the right-most value of the two
-  if (crossAbove(end) > crossBelow(end))
-    settling_idx = crossAbove(end);
+  if (amplifier_underswing > amplifier_output)
+      fprintf(1,'  Amplifier underswing larger than amplifier output...\n   skipping %s\n',measdir)
+      outvals_smooth = [];
+      settling_time = NaN;
   else
-    settling_idx = crossBelow(end);
-  end
-  settling_time = outtime_single(settling_idx) - outtime_single(idx);
+      % calculate settling time
+      % first attempt... smooth with 1/20th width sliding window
+      % note: this makes amp-max very inaccurate.
+      %       do not use outvals_smooth to derive amp-max!!
+      % 2020-02-18: if amp peak is too sharp, 1/20th wide is too broad, changed to 1/100th
+      smooth_span = round(numel(outvals_single)/100);
+      outvals_smooth = movmean(outvals_single,smooth_span);
+      % define the threshold (absolute value)
+      settling_time_threshold = abs(amplifier_output*settling_time_percent);
+      % the find point where it crosses above/below the threshold
+      crossAbove = find(outvals_smooth > (baseV+settling_time_threshold) );
+      crossBelow = find(outvals_smooth < (baseV-settling_time_threshold) );
+      % note: both variables SHOULD contain an array of index values, but if
+      % could also return empty, so ensure they always have a value
+      % perhaps this should throw an error instead?
+      if (numel(crossAbove) == 0) || (numel(crossBelow) == 0)
+          fprintf(1,'  Failed to find settling time for...\n   %s\n', measdir);
+          settling_time = NaN;
+      else
+          % take the right-most value of the two
+          if (crossAbove(end) > crossBelow(end))
+            settling_idx = crossAbove(end);
+          else
+            settling_idx = crossBelow(end);
+          end
+          settling_time = outtime_single(settling_idx) - outtime_single(idx);
+      end
+  end % if_statement: amplifier_underswing > amplifier_output
 
-  
+  % save this data into a master data cell
+  [~, measID, ~] = fileparts(measdir);
+  measID = str2num(strrep(measID,'meas',''));
+  wss.outtime_single = 1;
+  wss.invals_single = 2;
+  wss.outvals_single = 3;
+  wss.amplifier_input = 4;
+  wss.baseV = 5;
+  wss.amplifier_output = 6;
+  wss.outvals_smooth = 7;
+  wss.amplifier_underswing = 8;
+  waveform_save{measID} = {outtime_single, invals_single, outvals_single, amplifier_input, baseV, amplifier_output, outvals_smooth, amplifier_underswing};
+
   if debug
-      close all
-      figure(7)
-      plot(invals,'b')
+      figure(99)
+      plot(outtime_single,invals_single);
       hold on
-      plot(outvals,'r')
-      plot(in_falling_idx,invals(in_falling_idx),'kx')
-      plot(in_falling_idx(2)-10,invals(in_falling_idx(2)-10),'ro')
-      plot(in_falling_idx(3)-10,invals(in_falling_idx(3)-10),'go')
-      legend('invals','outvals','falling idx all','falling idx start','falling idx end')
-      hold off
-  
-      figure(8)
-      plot(outtime,outvals,'b')
-      hold on
-      plot(outtime_single,outvals_smooth,'r')
-      plot([outtime(1) outtime(end)],[baseV baseV],'k')
-      plot([outtime(1) outtime(end)],[baseV+settling_time_threshold baseV+settling_time_threshold],'c')
-      plot([outtime(1) outtime(end)],[baseV-settling_time_threshold baseV-settling_time_threshold],'c')
-      plot(outtime_single(crossAbove),outvals_smooth(crossAbove),'go')
-      plot(outtime_single(crossBelow),outvals_smooth(crossBelow),'kx')
-      plot(outtime_single(idx),outvals_smooth(idx),'rs')
-      legend('outvals','outvals smooth','threshold mean','threshold upper','threshold lower','values above','values below','inval_start')
+      plot(outtime_single(idx),invals_single(idx),'bx');
+      plot(outtime_single,trigger_single,'c');
+      plot(outtime_single,outvals_single,'r');
+      %plot(outtime(1:numel(outvals_smooth)),outvals_smooth,'g');
+      %plot([outtime_single(1) outtime_single(end)],[baseV+settling_time_threshold baseV+settling_time_threshold],'k');
+      %plot([outtime_single(1) outtime_single(end)],[baseV-settling_time_threshold baseV-settling_time_threshold],'k');
       hold off
   end
-  
 end % function analyze_oscope_amp
 
 
 function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   
   global gainfac;  % todo - remove this and gainfac below when no longer needed
+  global waveform_save;
+  global wss;
 
   q=load([ana_folder '/results.txt']);
   q=sortrows(q,1);
@@ -186,37 +225,6 @@ function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   settling_time = reshape(q(:,s.settime),[numel(m4b) numel(m2b)]);
   settling_time = settling_time'; % transpose to match the orientation of the simulations
   
-  
-%{
-  % very special patch to force 0-6V m2b/m4b sweep
-  % find index boundaries to slice the data with
-  % NOTE: plot_specific_meas does NOT work in this mode! (the rc->meas translation is wrong)
-  warning('Artifically shifting data to 0-6V... plot_specific_meas is NOT VALID in this mode!')
-  m2b_low  = find(m2b==0);
-  m2b_high = find(m2b==4);
-  m4b_low  = find(m4b==0);
-  m4b_high = find(m4b==4);
-  % artificially set the m2b/m4b to the correct ranges
-  m2b = 0:0.1:6;
-  m4b = 0:0.25:6;
-  % pre-alloc the new data
-  inV2           = nan([numel(m2b) numel(m4b)]);
-  outV2          = nan([numel(m2b) numel(m4b)]);
-  settling_time2 = nan([numel(m2b) numel(m4b)]);
-  % slice out the data
-  inV_slice           = inV(          m2b_low:m2b_high, m4b_low:m4b_high);
-  outV_slice          = outV(         m2b_low:m2b_high, m4b_low:m4b_high);
-  settling_time_slice = settling_time(m2b_low:m2b_high, m4b_low:m4b_high);
-  % stuff it into the array
-  inV2(          1:size(inV_slice,1),1:size(inV_slice,2))                     = inV_slice;
-  outV2(         1:size(outV_slice,1),1:size(outV_slice,2))                   = outV_slice;
-  settling_time2(1:size(settling_time_slice,1),1:size(settling_time_slice,2)) = settling_time_slice;
-  inV = inV2;
-  outV = outV2;
-  settling_time = settling_time2;
-%}
-  
-  
   % find the max gain and minimum settling time
   gain=outV ./ inV;
   maxgain=max(max(gain));
@@ -227,10 +235,6 @@ function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   [settime_r, settime_c]=find(settling_time==minsettime);
   if (numel(settime_r)>1); settime_r=settime_r(1); end
   if (numel(settime_c)>1); settime_c=settime_c(1); end
-  % report the "best" conditions (lowest settling time)
-  %best_gain = gain(settime_r,settime_c);
-  %best_time = settling_time(settime_r,settime_c);
-  
   
   % find the optimal operating point
   minV = 1.25;  maxV = 2;
@@ -243,12 +247,11 @@ function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   valid_settime(valid_settime==0) = NaN;
   [opt_r, opt_c] = find(valid_settime == min(min(valid_settime)));
   fprintf(1,'Number of opt_r = %d ; Number of opt_c = %d\n',numel(opt_r),numel(opt_c));
+  % TODO: find the opt_r and opt_c that isnt at edge of sweep range
   opt_r = opt_r(1);  opt_c = opt_c(1);
   
-
-  
   % plot metrics
-  pngpath = [ana_folder '/pngs'];
+  pngpath = [ana_folder '/analysis_pngs'];
   if ~exist(pngpath,'dir'); mkdir(pngpath); end
 
   % plot - gain
@@ -284,27 +287,11 @@ function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   end
   saveas(fh,sprintf('%s/sample_outputs.png',pngpath));
 
-  % plot - waveform of max gain
-  %now_r = gain_r;  now_c = gain_c;
-  %measID=meas_to_rc_translator(ana_folder,[now_r now_c]);
-  %fh=figure(5);
-  %plot_specific_meas(ana_folder,measID);
-  %title(sprintf('Waveform of max outV (meas %d - outV = %f; gain = %f; settling time = %f',measID,outV(now_r,now_c),gain(now_r,now_c),settling_time(now_r,now_c)))
-  %saveas(fh,sprintf('%s/gain_waveform.png',pngpath));
-  
-  % plot - waveform of lowest settling time
-  %now_r = settime_r;  settime_c = opt_c;
-  %measID=meas_to_rc_translator(ana_folder,[now_r now_c]);
-  %fh=figure(6);
-  %plot_specific_meas(ana_folder,measID);
-  %title(sprintf('Waveform of min settling time (meas %d - outV = %f; gain = %f; settling time = %f',measID,outV(now_r,now_c),gain(now_r,now_c),settling_time(now_r,now_c)))
-  %saveas(fh,sprintf('%s/settlingtime_waveform.png',pngpath));
-
   % plot - waveform of lowest settling time
   now_r = opt_r;  now_c = opt_c;
   custom_str = sprintf('Waveform of optimal point (minV=%0.2f)',minV);
   measID=meas_to_rc_translator(ana_folder,[now_r now_c]);
-  fh=figure(7);
+  fh=figure(5);
   plot_specific_meas(ana_folder,measID);
   % generate title string
   [~, ana_folder_ID, ~] = fileparts(ana_folder);
@@ -313,6 +300,12 @@ function [ m2b,m4b,gain,outV ] = generate_colormap(ana_folder)
   title({custom_str,sprintf('ana folder: %s',ana_folder_ID),stat_str1,stat_str2})
   saveas(fh,sprintf('%s/settlingtime_waveform.png',pngpath));
   
+  % save the analysis data for make_paper_plots
+  vars_to_save = {'m2b','m3b','m4b','q','s', ...
+                  'inV','outV','settling_time','gain', ...
+                  'valid_settime','opt_r','opt_c', 'measID', ...
+                  'waveform_save', 'wss'};
+  save([ana_folder '/ana_results.mat'],vars_to_save{:},'-v7');
 end % end-function generate_colormap
 
 
@@ -330,11 +323,15 @@ function [retval] = meas_to_rc_translator(ana_folder,inval)
     q=sortrows(q,1);
 
     s.measid=1;
-    s.m2br=2;
-    s.m3br=3;
-    s.m4br=4;
-    s.inval=5;
-    s.outval=6;
+    s.vcc=2;
+    s.gnd=3;
+    s.m2br=4;
+    s.m3br=5;
+    s.m4br=6;
+    s.m5br=7; % note: this is nothing for the amp
+    s.inval=8;
+    s.outval=9;
+    s.settime=10;
 
     m2b=sort(unique(q(:,s.m2br)));
     m3b=sort(unique(q(:,s.m3br)));
@@ -347,9 +344,17 @@ function [retval] = meas_to_rc_translator(ana_folder,inval)
         retval_c = find(m4b == res(4));
         retval = [retval_r retval_c m2b(retval_r) m4b(retval_c)];
     else
-        % translate an r/c pair into a meas num
-        m2b_val = m2b(inval(1));
-        m4b_val = m4b(inval(2));
+        if (   (floor(inval(1))==inval(1))  &&   (floor(inval(2))==inval(2))    )
+            % probably an rc index pair
+            % translate an r/c pair into a meas num
+            m2b_val = m2b(inval(1));
+            m4b_val = m4b(inval(2));
+
+        else
+            % probably is a specific bias setting
+            m2b_val = inval(1);
+            m4b_val = inval(2);
+        end
         res = q(   q(:,s.m2br)==m2b_val, : );
         res = res( res(:,s.m4br)==m4b_val, : );
         retval = res(1);
@@ -377,7 +382,7 @@ function plot_specific_meas(ana_folder,measID)
     else
         qin=load([ana_folder '/meas' sprintf('%04d',measID) '/ch1.csv.clean']);
     end
-    plot(q(:,1),q(:,2)-mean(q(:,2)),'LineWidth',1.5)
+    plot(q(:,1),q(:,2)-mean(q(:,2)),'-o','LineWidth',1.5)
     hold on
     plot(qin(:,1),qin(:,2)-abs(min(q(:,2)-mean(q(:,2))))-0.015,'r')
     hold off
--- SPC/analysis/analyze_comparator_meas.m
+++ SPC/analysis/analyze_comparator_meas.m
@@ -1,8 +1,12 @@
 
+global best_vbias;
+global best_vthresh;
 
-ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200122T164209_29D1-8_WP5_2-4-3_schmitt';  % first full acq of comparator
+%ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200122T164209_29D1-8_WP5_2-4-3_schmitt';  % first full acq of comparator
 %ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200127T171505_29D1-8_WP5_2-4-3_schmitt';  % second full acq of comparator
-ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200129T173850_29D1-8_WP5_2-4-3_schmitt';  % second full acq of comparator
+%ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200129T173850_29D1-8_WP5_2-4-3_schmitt';  best_vbias = 0.5; best_vthresh=0; % third full acq of comparator
+%ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200309T123730_29D1-8_WP5_2-4-3_schmitt';  best_vbias = 1; best_vthresh=3; % fourth full acq of comparator, 100 khz, improper load termination
+ana_folder = '/Volumes/ArrayData/MasdaX/2018-01/measurements/20200309T150149_29D1-8_WP5_2-4-3_schmitt';  best_vbias = 1; best_vthresh=3; % fourth full acq of comparator, 100 khz, fixed load termination
 
 addpath('./helper_functions');
 % clean the oscope data to make it matlab-friendly
@@ -11,6 +15,8 @@ clean_oscope_data(ana_folder);
 vb_dat = load([ana_folder '/vbiases.txt']);
 
 % walk through every meas and analyze
+global waveform_save;
+waveform_save={};
 ana_res_all = zeros([size(vb_dat,1) 6]);
 for fidx=1:size(vb_dat,1)
 %for fidx=50
@@ -28,12 +34,15 @@ alldat = [vb_dat ana_res_all];
 resfile = [ana_folder '/results.txt' ];
 csvwrite(resfile,alldat);
 
+close all
 generate_colormap(ana_folder);
-plot_specific_meas(ana_folder, [0.5 0])
 
 
 function [minout, maxout, deltaout, rising_thresh, falling_thresh, hysteresis] = analyze_comparator_oscope_data(measdir)
 
+  global waveform_save
+  global wss
+  
   % load in and out files
   in_raw=load([measdir '/ch1.csv.clean']);
   if exist([measdir '/math2.csv.clean'])
@@ -93,10 +102,26 @@ function [minout, maxout, deltaout, rising_thresh, falling_thresh, hysteresis] =
 %   hold off
 %   title(sprintf('%s',measdir))
 
+  % save this data into a master data cell
+  [~, measID, ~] = fileparts(measdir);
+  measID = str2num(strrep(measID,'meas',''));
+  wss.time = 1;
+  wss.invals = 2;
+  wss.outvals = 3;
+  wss.trig = 4;
+  wss.deltaout = 5;
+  wss.hysteresis = 6;
+  waveform_save{measID} = {time, invals, outvals, trig, deltaout, hysteresis};
 
 end
 
 function generate_colormap(ana_folder)
+
+  global waveform_save;
+  global wss;
+  global s;
+  global best_vbias;
+  global best_vthresh;
 
   [~, filename, ~] = fileparts(ana_folder);
   set(0,'defaultTextInterpreter','none');
@@ -104,19 +129,21 @@ function generate_colormap(ana_folder)
   q=load([ana_folder '/results.txt']);
   q=sortrows(q,1);
 
-  s.measid=1;
-  s.vcca=2;
-  s.gnda=3;
-  s.vccd=4;
-  s.gndd=5;
-  s.vbias=6;
-  s.vthresh=7;
-  s.minout=8;
-  s.maxout=9;
-  s.deltaout=10;
-  s.rising_thresh=11;
-  s.falling_thresh=12;
-  s.hysteresis=13;
+  if (numel(s) == 0)
+    s.measid=1;
+    s.vcca=2;
+    s.gnda=3;
+    s.vccd=4;
+    s.gndd=5;
+    s.vbias=6;
+    s.vthresh=7;
+    s.minout=8;
+    s.maxout=9;
+    s.deltaout=10;
+    s.rising_thresh=11;
+    s.falling_thresh=12;
+    s.hysteresis=13;
+  end
   
   vbias=sort(unique(q(:,s.vbias)));
   vthresh=sort(unique(q(:,s.vthresh)));
@@ -124,33 +151,73 @@ function generate_colormap(ana_folder)
   % this reshape actually covers up the missing SMU data points!
   deltaout = reshape( q(:,s.deltaout),[numel(vthresh) numel(vbias)]);
   hysteresis = reshape( q(:,s.hysteresis),[numel(vthresh) numel(vbias)]);
+  % transpose to the "slower changing" dimension is in the y-direction
+  deltaout = deltaout';
+  hysteresis = hysteresis';
   
-  figure
-  imagesc(vbias,vthresh,deltaout); colorbar
+  % create folder to save analysis PNGs
+  pngpath = [ana_folder '/analysis_pngs'];
+  if ~exist(pngpath,'dir'); mkdir(pngpath); end
+  
+  % find the "best point"
+  % currently defined as hysteresis between 0.25 and 0.5V
+  % and then maximum delta out
+  valid_idx1 = (hysteresis>0.25);
+  valid_idx2 = (hysteresis<0.5);
+  valid_idx = valid_idx1 .* valid_idx2;
+  valid_deltaout = deltaout .* valid_idx;
+  valid_deltaout(valid_deltaout==0) = NaN;
+  [opt_r, opt_c] = find(valid_deltaout == max(max(valid_deltaout)));
+  fprintf(1,'Number of opt_r = %d ; Number of opt_c = %d\n',numel(opt_r),numel(opt_c));
+  opt_r = opt_r(1);  opt_c = opt_c(1);
+  % note: opt_r and opt_c algorithm doesnt work well right now, assuming hard coded values
+  opt_r = find(vbias == best_vbias);
+  opt_c = find(vthresh == best_vthresh);
+  
+  % find the measID of opt_r and opt_c
+  % note: this is using a different method than analyze_amplifier_meas uses
+  valid_idx1 = find(q(:,s.vbias)==vbias(opt_r));
+  valid_idx2 = find(q(valid_idx1,s.vthresh)==vthresh(opt_c));
+  valid_idx = valid_idx1(valid_idx2);
+  measID = q(valid_idx,1);
+  
+  figure(1)
+  plot_specific_meas(ana_folder, [vbias(opt_r) vthresh(opt_c)]);
+  
+  figure(2)
+  % faster changing dimension (vthresh) goes first (x-dir)
+  imagesc(vthresh,vbias,deltaout); colorbar
   set(gca,'YDir','normal');
   xlabel('Vthresh (V)')
   ylabel('Vbias (V)')
   colormap jet
   title(sprintf('Delta between comparator off and on (%s)', filename))
   caxis([0 8])
-  saveas(gcf,[ana_folder '/deltaout.png']);
+  saveas(gcf,[pngpath '/deltaout.png']);
 
-  
-  figure
-  imagesc(vbias,vthresh,hysteresis); colorbar
+  figure(3)
+  imagesc(vthresh,vbias,hysteresis); colorbar
   set(gca,'YDir','normal');
   xlabel('Vthresh (V)')
   ylabel('Vbias (V)')
   colormap jet
   title(sprintf('Hysteresis of comparator (%s)', filename))
   caxis([0 2.5])
-  saveas(gcf,[ana_folder '/hysteresis.png']);
+  saveas(gcf,[pngpath '/hysteresis.png']);
   
-
+  
+  % save the analysis data for make_paper_plots
+  vars_to_save = {'vbias','vthresh','q','s', ...
+                  'deltaout','hysteresis', ...
+                  'opt_r','opt_c', 'measID', ...
+                  'waveform_save', 'wss'};
+  save([ana_folder '/ana_results.mat'],vars_to_save{:},'-v7');
 
 end
 
 function plot_specific_meas(ana_folder, input)
+
+  global s;
 
   [~, filename, ~] = fileparts(ana_folder);
   set(0,'defaultTextInterpreter','none');
@@ -158,25 +225,11 @@ function plot_specific_meas(ana_folder, input)
   q=load([ana_folder '/results.txt']);
   q=sortrows(q,1);
 
-  s.measid=1;
-  s.vcca=2;
-  s.gnda=3;
-  s.vccd=4;
-  s.gndd=5;
-  s.vbias=6;
-  s.vthresh=7;
-  s.minout=8;
-  s.maxout=9;
-  s.deltaout=10;
-  s.rising_thresh=11;
-  s.falling_thresh=12;
-  s.hysteresis=13;
-
   if (numel(input)==2)
-      vbias = input(1)
-      vthresh = input(2)
-      idx1 = (q(:,s.vbias)==vbias)
-      idx2 = (q(:,s.vthresh)==vthresh)
+      vbias = input(1);
+      vthresh = input(2);
+      idx1 = (q(:,s.vbias)==vbias);
+      idx2 = (q(:,s.vthresh)==vthresh);
       idx = find( (idx1 .* idx2) == 1 );
   else
       idx = input;
@@ -190,12 +243,12 @@ function plot_specific_meas(ana_folder, input)
       qout = load([measdir '/ch2.csv.clean']);
   end
   
-  figure
+  %figure
   plot(qin(:,1),qin(:,2))
   hold on
   plot(qout(:,1),qout(:,2),'r')
   hold off
-  saveas(gcf,[ana_folder sprintf('/meas%04d.png',idx)]);
+  saveas(gcf,[ana_folder sprintf('/analysis_pngs/meas%04d.png',idx)]);
 
 
 end
