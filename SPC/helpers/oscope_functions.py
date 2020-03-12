7dafbea5fb1a8e0393228914b3d1b355783bdf39
none <none@none.com>
2020-03-12 10:19:56 -0400

    added function to set record length on th oscope

--- SPC/helpers/oscope_functions.py
+++ SPC/helpers/oscope_functions.py
@@ -2,7 +2,7 @@
 import time
 
 
-def run(mi, acq_delay):
+def acq_delayer(mi, acq_delay):
     print("  Waiting for %s acquisitions before saving file" % acq_delay)
     acqstart = int(mi.query("ACQ:NUMACQ?"))
     acqnow = acqstart
@@ -15,3 +15,14 @@ def run(mi, acq_delay):
         acqnow = int(mi.query("ACQ:NUMACQ?"))
         print("%d" % (acqnow - acqstart), end="...")
     print("Reached at least %d acqs!" % acq_delay)
+
+
+def set_recordlength(mi, val):
+    print("  Setting record length to %d" % val)
+    curr_recordlength = float(mi.query("HORIZONTAL:MODE:RECORDLENGTH?"))
+    if curr_recordlength != val:
+        samplerate_gainfac = val / curr_recordlength
+        new_samplerate = float(mi.query("HORIZONTAL:MODE:SAMPLERATE?")) * samplerate_gainfac
+        mi.write("HORIZONTAL:MODE:SAMPLERATE %f" % new_samplerate)
+
+
