c216be1a86129653f69967b2064d349cd4261000
none <none@none.com>
2020-07-22 00:14:30 -0400

    broke out acq_delay to be individual per measurement type since clockgen CR measurements dont need math averaging, added more values to voltage_db for CR measurements

--- SPC/meas_characterize_general.py
+++ SPC/meas_characterize_general.py
@@ -14,21 +14,26 @@ import meas_characterize_countrate
 SPCsetup_path = "../SPCsetup1"
 scopeip = "192.168.66.85"
 unixdir = "/mnt/ArrayData/MasdaX/2018-01/measurements"
-chipID = "29D1-8_WP8_4-6-10_2SR3inv"
+chipID = "29D1-5_WP6_3-3-1_2SR3inv"
 
 #meas_type = "amp"
 #runcon = "custom step wave 200 Hz 130 mVpp with 1:10 voltage divider, effective 13 mVpp, horiz acq is 10k samples"
 #notes = "added 50ohm load to siggen input to probe card, running standard comp sweep at 100 kHz"
+acq_delay = 400
 
 #meas_type = "comp"
 #cirtype = "schmitt"
 #runcon = "ramp 0-4.5V 100khz, 1.8MEG 12c probe with calibrated gain of 16 (24 dB)"
 #notes = "re-running standard comp sweep with high ramp max so hysteresis curve looks more symmetrical"
+acq_delay = 400
 
+# note: there is no meas_type clockgen, but this label is here for clarity
 meas_type = "clockgen"
 meas_type = "countrate"
-runcon = "square 0-5V, (2x) 1.8MEG 12c probe with calibrated gain of 16 (24 dB) and 19 (25.6 dB)"
-notes = "third count rate meas of a good circuit with wider oscope div window and higher freq"
+runcon = "square 0-5V, (2x) 1.8MEG 12c probe with calibrated gain of 16 (24 dB) and 18 (25.1 dB)"
+#notes = "third count rate meas of a good circuit with wider oscope div window and higher freq"
+notes = "properly working pretty much exactly 100 kcps"
+acq_delay = 50 # afraid to set this to 0, but technically it should be
 
 #meas_type = "counter"
 
@@ -41,18 +46,18 @@ target_recordlength = 10000
 # FORMAT:  math<#> = (ch_num, command, num_samples)
 math_ch = (2, "AVG(CH2)", 300)
 # TODO: consider moving this value into each respective measurement (amp, comp, etc)
-acq_delay = 400  # purposely waiting an extra 100 acq
+#acq_delay = 400  # purposely waiting an extra 100 acq
 
 
 # ---- Begin measurement script ---- #
 print("""
 Checklist before starting:
 - Is "./multim_2636A_unified.sh SPCsetup1" is running?
-- Is eve proxy running? (ssh -D 9998 eve)
 - Did you mount psidata on the oscilloscope?
 - Did you update chipID, runcon, and notes?
 - Are all the desired channels and maths turned on? (eg, ch1 ch2 math1 math2)
 - Are all impedances set correctly?  Atten factor applied to scope?
+- For count rate measurements, did you add this circuit to the db?
 """)
 input("Press ENTER to continue...")
 
@@ -67,6 +72,7 @@ measdir = "%s/%s_%s" % (unixdir, dtag, chipID)
 os.mkdir(measdir)
 
 # write the SPCsetup[12] information
+# TODO: hostname is probably not needed anymore since smu_data doesnt have the hostname variable in the SPCsetup1 file (it probably did in the past so the substition needed to be done here)
 smu_data = subprocess.check_output(
     "grep DFILEPREFIX %s | grep -v '#' | tail -n 1 | sed -e 's/.*=//' -e 's/\"//g'" % SPCsetup_path, shell=True)
 hostname = subprocess.check_output(
@@ -107,14 +113,16 @@ if meas_type == "amp":
 elif meas_type == "comp":
     mi.write("MATH1:DEF \"Ch1\"")
     meas_characterize_comp.run(mi, measdir, smu_data, acq_delay, cirtype)
-elif meas_type == "clockgen":
-    meas_characterize_clockgen.run(mi, measdir, smu_data)
+#elif meas_type == "clockgen":
+#    meas_characterize_clockgen.run(mi, measdir, smu_data)
 elif meas_type == "counter":
     meas_characterize_counter.run(mi, measdir, smu_data)
 elif meas_type == "countrate":
     voltage_db = {
         "29D1-8_WP5_2-4-3_schmitt": ["v1(8)", "v2(0)", "v3(8)", "v4(0)", "v5(1)", "v6(3)", "comp"],
         "29D1-8_WP8_4-6-10_2SR3inv": ["v1(8)", "v2(0)", "v3(0)", "v4(0)", "v5(0)", "v6(0)", "clockgen"],
+        "29D1-5_WP6_3-2-16_2SR3inv": ["v1(8)", "v2(0)", "v3(0)", "v4(0)", "v5(0)", "v6(0)", "clockgen"],
+        "29D1-5_WP6_3-3-1_2SR3inv": ["v1(8)", "v2(0)", "v3(0)", "v4(0)", "v5(0)", "v6(0)", "clockgen"],
     }
     if chipID not in voltage_db.keys():
         print("Error: no voltage data entry found for %s" % chipID)
